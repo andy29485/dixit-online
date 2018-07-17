d3c7891997ac8989eba7471de412da62aec787e8
Andriy Zasypkin <AndriyZasypkin@gmail.com>
2018-07-17 18:21:56 -0400

    added russian translation

--- locales/ru.js
+++ locales/ru.js
@@ -1,1 +1,134 @@
-{}
+{
+  "name":      "Имя",
+  "email":     "Email",
+  "language":  "Язык",
+  "ex_uname":  "имя пользователя",
+  "ex_email":  "email@example.com",
+  "username":  "имя пользователя",
+  "password":  "Пароль",
+  "old_pass":  "Старый пароль",
+  "conf_pass": "Подтверждение пароля",
+  "sign_up":   "Регистрация",
+  "sign_in":   "Вход",
+
+  "error":           "Ошибка!",
+  "success":         "Успешно!",
+  "game_not_exists": "Игра не существует",
+  "not_in_game":     "Вы в этой игре не участвуете",
+  "pass_missmatch":  "пароли не совпадают",
+  "pass_short":      "пароль должен содержать минимум %d символов",
+  "uname_short":     "имя пользователя должно содержать минимум %d символов",
+  "uname_exist":     "такой пользователь уже существует",
+  "uname_not_found": "пользователь не найден",
+  "incorrect_pass":  "неверный пароль",
+
+  "name_updated":        "Имя изменено",
+  "lang_updated":        "настройки языка изменены",
+  "email_updated":       "Email изменен",
+  "password_updated":    "Пароль изменен",
+  "password_missing":    "Не введен старый пароль",
+  "password_incorect":   "Неверный старый пароль",
+  "settings_save_error": "При сохранении произошла ошибка, попробуйте еще раз",
+
+  "title":    "Dixit Online",
+  "profile":  "Профиль",
+  "settings": "Настройки",
+  "logout":   "Выход",
+  "profile_head": "Профиль %s",
+  "create_new":   "Создать новую игру",
+
+  "active_games":     "Активные игры",
+  "joinable_games":   "Доступные игры",
+  "archived_games":   "Завершенные игры",
+  "none_found":       "Не найдено",
+  "more_results":     "more results",
+  "first_page":       "Первый",
+  "last_page":        "Последний",
+  "no_limit":         "Без ограничения",
+  "no_lim_0":         "(от 0 до)",
+  "admin_tag":        "(администратор)",
+  "max_players":      "Максимальное число игроков",
+  "delete_explain":   "Удалить игру со всеми записями",
+  "delete_not_admin": "Вы не являетесь администратором этой игры",
+
+  "game_name":      "Название игры",
+  "stage_duration": "Длительность игры",
+  "duration_days":  "дней.",
+  "stage":          "Этап игры",
+  "admin":          "Администратор",
+  "players_and_limit": "Игроков в игре / Максимум",
+  "delete":            "Удалить",
+  "duration_exp":
+    "Интервал между этапами (Подключение игроков, выбор ассоциаций, выбор карт)",
+  "decks_to_use": "Колоды",
+
+  "scoring_method": "Метод подсчета",
+  "scoring": {
+    "orig":    "Обычный",
+    "updated": "Продвинутый",
+    "russian": "Русский"
+  },
+
+  "end_date_join":     "Подключение до",
+  "end_date_join_alt": "Регистрация закрыта",
+  "end_date_stage":    "Этап заканчивается до",
+  "end_date_end":      "Игра заканчивается до",
+
+  "join_button":     "Подключиться к игре",
+  "leave_button":    "Покинуть игру",
+  "next_button":     "Дальше",
+  "edit_button":     "Редактировать",
+  "deadline_button": "Установить длительность игры",
+  "start_button":    "Закончить настройку",
+  "submit_button":   "Отправить",
+
+  "header_number":         "#",
+  "header_name":           "Имя",
+  "header_status":         "Статус",
+  "header_players_list":   "Игроки в игре",
+  "header_include_player": "Добавить",
+
+  "game_full":        "Достигнуто максимальное число игроков",
+  "game_full_long":   "Регистрация окончена, новые игроки не смогут подключиться",
+  "deadline_reached": "Продлить игру",
+  "lacking_players":  "Недостаточно  игроков",
+  "status_waiting":   "Ожидание",
+  "status_done":      "Завершено",
+
+  "caption":     "Ассоциация",
+  "explanation": "Пояснение",
+
+  "hand_ownership": "карты %s",
+  "total_scores":   "Итого",
+  "player":         "Игрок",
+  "score":          "Счет",
+  "round":          "Круг",
+  "dealer_name":    "Ведущий",
+  "dealer_caption": "Aссоциация Ведущего",
+  "dealer_card":    "Карта Ведущего",
+  "other_cards":    "Карты",
+  "votes_score":    "голоса / счет",
+  "player_card":    "карта",
+  "player_name":    "имя",
+  "voted_for":      "Проголосовали за: %s",
+  "no_one":         "<Никто>",
+  "no_explain":     "<Нет объяснения>",
+
+  "stages": {
+  "join":   "Подключение",
+  "wait":   "Ожидание",
+  "capt":   "Выбор ассоциаций",
+  "choice": "Выбор карт",
+  "vote":   "Угадывание карт",
+  "end":    "Завершено"
+  },
+
+  "email_messages": {
+    "capt":   "Игра началась, выберите карту и придумайте ассоциацию:",
+    "choice": "Игра продолжается, подберите карты к ассоциациям других игроков:",
+    "vote":   "Игра близится к концу, угадайте какую карту загадал каждый участник:",
+    "end":    "Игра окончена, результаты можно посмотреть здесь:"
+  },
+
+  "subject": "%s продвинулась на следующий этап"
+}
